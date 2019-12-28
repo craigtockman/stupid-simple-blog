339c370e425c30833f5d199442ec50eb147bec27
craig tockman <craigtockman@gmail.com>
2019-12-28 00:46:53 -0800

    Update about

--- pages/about.js
+++ pages/about.js
@@ -14,21 +14,36 @@ export default function About() {
         <h1>About Stupid Simple Blog</h1>
         <h2>Tell your mom about this website and thanks for visiting.</h2>
         <p>
-          Learn how to build this site here:{' '}
+          Learn how to build this site here:
+          <br />
           <a href='https://medium.com/@craigory/build-a-stupid-simple-serverless-next-js-blog-f2bb84f1e7e'>
-            Click here
+            https://medium.com/@craigory/build-a-stupid-simple-serverless-next-js-blog-f2bb84f1e7e
           </a>
         </p>
+        <h2>What's included</h2>
+        <ul>
+          <li>Continuous deployment with Netlify, React.js and Next.js</li>
+          <li>A Netlify blog post form.</li>
+          <li>Seo friendly dynamic meta-tag component.</li>
+          <li>RSS and Sitemap xml generator.</li>
+          <li>
+            Infinite scroll homepage that queries the content posted to Netlify
+            from the form.
+          </li>
+          <li>Material UI styles.</li>
+          <li>Responsive styles and media queries.</li>
+          <li>A simple pagination pager.</li>
+          <li>Email notifications of new blog posts.</li>
+          <li>React hooks only up in here.</li>
+          <li>A sweet header that follow you down the screen.</li>
+        </ul>
         <style jsx>
           {`
             li {
-              margin: 40px 0;
+              margin: 4px 0;
             }
-            ol {
-              margin-left: 20px;
-            }
-            img {
-              margin-top: 10px;
+            ul {
+              margin-left: 16px;
             }
           `}
         </style>
