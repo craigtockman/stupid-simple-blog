d5a2fc4b6fcdf23abffa586aa5817824fafb7ced
craig tockman <craigtockman@gmail.com>
2019-12-31 00:15:40 -0800

    fix rss

--- scripts/postExport.js
+++ scripts/postExport.js
@@ -8,7 +8,7 @@ const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
   .map(
     path => `
   <url>
-    <loc>https://stupid-simple-blog.com${path}</loc>
+    <loc>https://stupidsimple.blog${path}</loc>
     <lastmod>${dbSitemap[path].publishedSitemap}</lastmod>
   </url>`
   )
@@ -20,21 +20,21 @@ fs.writeFileSync('public/sitemap.xml', sitemapXml)
 const rssXml = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
-      <atom:link href="https://stupid-simple-blog.com/rss.xml" rel="self" type="application/rss+xml" />
+      <atom:link href="https://stupidsimple.blog/rss.xml" rel="self" type="application/rss+xml" />
       <title>Stupid Simple Blog.</title>
-      <link>https://stupid-simple-blog.com</link>
+      <link>https://stupidsimple.blog</link>
       <description>A Stupid Simple Blog demo witn next.js.</description>
       <image>
-          <url>https://stupid-simple-blog.com/apple-touch-icon-152x152.png</url>
+          <url>https://stupidsimple.blog/apple-touch-icon-152x152.png</url>
           <title>Stupid Simple Blog.</title>
-          <link>https://stupid-simple-blog.com</link>
+          <link>https://stupidsimple.blog</link>
           <description>Stupid Simple Blog. Logo</description>
       </image>${Object.keys(dbSitemap)
         .map(
           path => `
         <item>
             <title>${dbSitemap[path].title}</title>
-            <guid>https://stupid-simple-blog.com${path}</guid>
+            <guid>https://stupidsimple.blog${path}</guid>
             <description>${dbSitemap[path].description}</description>
             <pubDate>${dbSitemap[path].publishedRSS}</pubDate>
         </item>`
